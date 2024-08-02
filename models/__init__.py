b78e3efc5519b129291ff566bfc35194e435d9bb
JinY97 <109841505+JinY97@users.noreply.github.com>
2024-08-02 17:31:12 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,6 @@ Author: JinYin
 Date: 2022-08-29 20:34:00
 LastEditors: JinYin
 LastEditTime: 2023-07-18 19:23:32
-FilePath: \07_CTTNet\models\__init__.py
 '''
 from models.Models import FCNN, RNN, NovelCNN, SimpleCNN
 from models.DuoCL import DuoCL

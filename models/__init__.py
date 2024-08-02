5402af01f4ce71a7d9f72bb454d95658d4e88547
JinY97 <109841505+JinY97@users.noreply.github.com>
2024-08-02 17:19:06 +0800

    Add files via upload

---
+++ models/__init__.py
@@ -0,0 +1,11 @@
+'''
+Author: JinYin
+Date: 2022-08-29 20:34:00
+LastEditors: JinYin
+LastEditTime: 2023-07-18 19:23:32
+FilePath: \07_CTTNet\models\__init__.py
+'''
+from models.Models import FCNN, RNN, NovelCNN, SimpleCNN
+from models.DuoCL import DuoCL
+from models.GCTNet import Generator
+from models.STFNet import STFNet
